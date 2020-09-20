ac6b458122bda81f08bd43e4b57389cf60986d09
Jerry <calljerryli@outlook.com>
2020-09-20 16:18:01 +0800

    feat idle-monster

---
+++ idle-monster/precache-manifest.598300fa3e5cbc2ca75cb43fc3f4f7c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "171c75f68e8bbc031b8d3430eb82619c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "946ffedb3bd0f603aea4",
+    "url": "./static/css/main.d7182fb3.chunk.css"
+  },
+  {
+    "revision": "2c493f5cb523f51aae1f",
+    "url": "./static/js/2.78e3543b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.78e3543b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946ffedb3bd0f603aea4",
+    "url": "./static/js/main.06a473c8.chunk.js"
+  },
+  {
+    "revision": "29a1561020daf2b8e4c9",
+    "url": "./static/js/runtime-main.71939466.js"
+  }
+]);
